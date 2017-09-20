8aae1a15271b297fe180bf194b820cac7d7c2400
alexandrebarbaruiva <lexander.santos@gmail.com>
2017-09-20 09:40:07 -0300

    Fixing hpp

--- string_soma.hpp
+++ string_soma.hpp
@@ -1,5 +1,6 @@
 #import <iostream>
 
-std::string remove_spaces(my_string);
+std::string remove_newlines(std::string my_string);
 std::vector<int> vectorize(std::string my_string);
+std::string find_delimiters(std::string my_string);
 int soma_string(std::string my_string);
